a351f1d84f737ea4ea28fc5c2e340faebca35b29
Damillora <damillora@damillora.com>
2023-04-01 02:10:37 +0700

    feat: notice component

---
+++ src/lib/components/Notice/Notice.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+<div class="notice">
+	<slot />
+</div>
+
+<style lang="scss">
+	@import '../../styles/global';
+
+	.notice {
+		background: var(--primary-bg-color);
+		color: var(--primary-color);
+		padding: 1.25rem;
+		border: 1px solid var(--primary-color);
+	}
+</style>
---
+++ src/routes/examples/components/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import PostCard from '$lib/components/PostCard/PostCard.svelte';
+	import Container from '$lib/components/Container/Container.svelte';
+	import Index from '$lib/components/PageTypes/Index.svelte';
+	import Notice from '$lib/components/Notice/Notice.svelte';
+	import Post from '$lib/components/PageTypes/Post.svelte';
+</script>
+
+<Container>
+	<Index>
+		<Post>
+			<h2>Notice</h2>
+			<Notice>
+				This page is a <strong>work-in-progress</strong>. Contributions are welcome!
+			</Notice>
+		</Post>
+	</Index>
+</Container>
