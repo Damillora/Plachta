8e6ae0567cbcb9904be190863db2e9ae6a00213b
Damillora <damillora@damillora.com>
2023-08-08 19:48:51 +0700

    feat: add index exports

---
+++ src/lib/index.ts
@@ -0,0 +1,86 @@
+// Base
+import Base from './components/Base/Base.svelte';
+import GhostStyle from './components/Base/GhostStyle.svelte';
+
+// Card
+import Card from './components/Card/Card.svelte';
+import CardBanner from './components/Card/CardBanner.svelte';
+import CardContent from './components/Card/CardContent.svelte';
+import CardDescription from './components/Card/CardDescription.svelte';
+import CardFooter from './components/Card/CardFooter.svelte';
+import CardTitle from './components/Card/CardTitle.svelte';
+
+// CardList
+import CardList from './components/CardList/CardList.svelte';
+
+// Container
+import Container from './components/Container/Container.svelte';
+
+// Footer
+import Footer from './components/Footer/Footer.svelte';
+
+// Header
+import Header from './components/Header/Header.svelte';
+
+// Hero
+import Hero from './components/Hero/Hero.svelte';
+
+// Nav
+import Nav from './components/Nav/Nav.svelte';
+import NavDarkMode from './components/Nav/NavDarkMode.svelte';
+import NavMenu from './components/Nav/NavMenu.svelte';
+import NavMenuBase from './components/Nav/NavMenuBase.svelte';
+import NavSearch from './components/Nav/NavSearch.svelte';
+
+// NavigationLoading
+import NavigationLoading from './components/NavigationLoading/NavigationLoading.svelte';
+
+// Notice
+import Notice from './components/Notice/Notice.svelte';
+
+// PageHeader
+import PageHeader from './components/PageHeader/PageHeader.svelte';
+
+// PageTypes
+import Index from './components/PageTypes/Index.svelte';
+import Post from './components/PageTypes/Post.svelte';
+
+// Post
+import AuthorHeader from './components/Post/AuthorHeader.svelte';
+import PostHeader from './components/Post/PostHeader.svelte';
+import PostMain from './components/Post/PostMain.svelte';
+import PostNavigator from './components/Post/PostNavigator.svelte';
+import PostRelated from './components/Post/PostRelated.svelte';
+import TagHeader from './components/Post/TagHeader.svelte';
+
+// PostCard
+import PostCard from './components/PostCard/PostCard.svelte';
+import PostCardAuthor from './components/PostCard/PostCardAuthor.svelte';
+import PostCardInfo from './components/PostCard/PostCardInfo.svelte';
+import SearchCard from './components/PostCard/SearchCard.svelte';
+
+export {
+    Base, GhostStyle,
+    Card, CardBanner, CardContent, CardDescription, CardFooter, CardTitle,
+    CardList,
+    Container,
+    Footer,
+    Header,
+    Hero,
+    Nav, NavDarkMode,NavMenu,NavMenuBase,NavSearch,
+    NavigationLoading,
+    Notice,
+    PageHeader,
+    Index,
+    Post,
+    AuthorHeader,
+    PostHeader,
+    PostMain,
+    PostNavigator,
+    PostRelated,
+    TagHeader,
+    PostCard,
+    PostCardAuthor,
+    PostCardInfo,
+    SearchCard
+};
